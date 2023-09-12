2e6d910356d0db98295787c458f4e0a0a98d4f2d
Martin Ramm <martin-ramm@gmx.de>
2023-09-12 22:38:14 +0200

    Make propertyIsFunction compatible with interface definitions

--- lib/core/compiler-plugin/util/propertyIsFunction.ts
+++ lib/core/compiler-plugin/util/propertyIsFunction.ts
@@ -1,4 +1,4 @@
-import { Symbol as TSMorphSymbol, SyntaxKind } from 'ts-morph';
+import {Symbol as TSMorphSymbol, SyntaxKind} from 'ts-morph';
 
 export const propertyIsFunction = (property: TSMorphSymbol) => {
   const valueDeclaration = property.getValueDeclaration();
@@ -6,21 +6,22 @@ export const propertyIsFunction = (property: TSMorphSymbol) => {
     return false;
   }
 
-  if (
-    'initializer' in valueDeclaration.compilerNode &&
-    valueDeclaration.compilerNode.initializer !== undefined &&
-    typeof valueDeclaration.compilerNode.initializer === 'object' &&
-    valueDeclaration.compilerNode.initializer !== null &&
-    'kind' in valueDeclaration.compilerNode.initializer
-  ) {
+  if (valueDeclaration.isKind(SyntaxKind.PropertyAssignment)) {
     const initializer = valueDeclaration.compilerNode.initializer;
     return (
       initializer.kind === SyntaxKind.FunctionExpression || //{ a: function(){...} }
-      initializer.kind === SyntaxKind.ArrowFunction
-    ); //{ a: () => {...} }
+      initializer.kind === SyntaxKind.ArrowFunction // { a: () => {...} }
+    );
   }
+
+  if (valueDeclaration.isKind(SyntaxKind.PropertySignature)) {
+    return valueDeclaration.compilerNode.type !== undefined
+      && valueDeclaration.compilerNode.type.kind === SyntaxKind.FunctionType; //interface I { a: () => string; }
+  }
+
   return (
+    valueDeclaration.isKind(SyntaxKind.MethodSignature) || //interface I { a(): string; }
     valueDeclaration.isKind(SyntaxKind.MethodDeclaration) || //{ a() {...} }
-    valueDeclaration.isKind(SyntaxKind.SetAccessor)
-  ); //{ set a(a: ...) {...} }
+    valueDeclaration.isKind(SyntaxKind.SetAccessor) //{ set a(a: ...) {...} } OR interface I { set a(a: ...); }
+  );
 };
--- lib/core/test/compiler-plugin/util/propertyIsFunction.ts
+++ lib/core/test/compiler-plugin/util/propertyIsFunction.ts
@@ -11,29 +11,135 @@ const testCases: Array<[string, boolean]> = [
   ['e', true],
   ['f', false],
 ];
-
 typeTestCases.forEach(([type, prefix]) =>
   test(type, t => {
     const sourceFile = tsMorphProject.createSourceFile(
       'test.ts',
-      `${prefix}\nexport const test = {a: null as unknown as ${type}, b: function(){return true;}, c: () => true, d() {return true;}, set e(newE: number) {}, f: "str"};`,
-      { overwrite: true }
+      `${prefix}\n` +
+      `export const test = {a: null as unknown as ${type}, b: function(){return true;}, c: () => true, d() {return true;}, set e(newE: number) {}, f: "str"};` +
+      `export interface Test { a: ${type}; b: () => true, c(): void; d(): true; set e(new E: number); f: string;}`,
+      {overwrite: true}
     );
 
     const testVariable = sourceFile.getVariableDeclaration('test');
     if (testVariable === undefined) {
+      t.fail('Variable not found');
+      return;
+    }
+
+    const testInterface = sourceFile.getInterface('Test');
+    if (testInterface === undefined) {
       t.fail('Interface not found');
       return;
     }
 
-    testCases.forEach(([propertyKey, expectedResult]) => {
-      const property = testVariable.getType().getProperty(propertyKey);
-      if (property === undefined) {
-        t.fail(`Cannot find property ${propertyKey}`);
-        return;
-      }
-      const actualResult = propertyIsFunction(property);
-      t.is(actualResult, expectedResult, `property ${propertyKey}`);
-    });
+    [testVariable, testInterface].forEach(node =>
+      testCases.forEach(([propertyKey, expectedResult]) => {
+        const property = node.getType().getProperty(propertyKey);
+        if (property === undefined) {
+          t.fail(`Cannot find property ${propertyKey}`);
+          return;
+        }
+        const actualResult = propertyIsFunction(property);
+        t.is(actualResult, expectedResult, `${node.getName()}.${propertyKey}`);
+      })
+    )
   })
 );
+
+const builtInObjectsTestCases = {
+  Date: [
+    'toString',
+    'toDateString',
+    'toTimeString',
+    'toLocaleString',
+    'toLocaleDateString',
+    'toLocaleTimeString',
+    'valueOf',
+    'getTime',
+    'getFullYear',
+    'getUTCFullYear',
+    'getMonth',
+    'getUTCMonth',
+    'getDate',
+    'getUTCDate',
+    'getDay',
+    'getUTCDay',
+    'getHours',
+    'getUTCHours',
+    'getMinutes',
+    'getUTCMinutes',
+    'getSeconds',
+    'getUTCSeconds',
+    'getMilliseconds',
+    'getUTCMilliseconds',
+    'getTimezoneOffset',
+    'setTime',
+    'setMilliseconds',
+    'setUTCMilliseconds',
+    'setSeconds',
+    'setUTCSeconds',
+    'setMinutes',
+    'setUTCMinutes',
+    'setHours',
+    'setUTCHours',
+    'setDate',
+    'setUTCDate',
+    'setMonth',
+    'setUTCMonth',
+    'setFullYear',
+    'setUTCFullYear',
+    'toUTCString',
+    'toISOString',
+    'toJSON',
+  ],
+  String: [
+    'toString',
+    'charAt',
+    'charCodeAt',
+    'concat',
+    'indexOf',
+    'lastIndexOf',
+    'localeCompare',
+    'match',
+    'replace',
+    'search',
+    'slice',
+    'split',
+    'substring',
+    'toLowerCase',
+    'toLocaleLowerCase',
+    'toUpperCase',
+    'toLocaleUpperCase',
+    'trim',
+    'substr',
+    'valueOf',
+  ]
+}
+
+Object.entries(builtInObjectsTestCases).forEach(([objectName, functionNames]) =>
+  functionNames.forEach(functionName =>
+    test(`${objectName}.${functionName}`, t => {
+      const sourceFile = tsMorphProject.createSourceFile(
+        'test.ts',
+        `export const test = new ${objectName}();`,
+        { overwrite: true }
+      );
+
+      const testVariable = sourceFile.getVariableDeclaration('test');
+      if (testVariable === undefined) {
+        t.fail('Interface not found');
+        return;
+      }
+
+      const property = testVariable.getType().getProperty(functionName);
+      if (property === undefined) {
+        t.fail(`Cannot find property ${functionName}`);
+        return;
+      }
+
+      const actualResult = propertyIsFunction(property);
+      t.is(actualResult, true, `property ${functionName}`);
+    })
+  )
+)
